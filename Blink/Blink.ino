08da80e24c9ce4cbb2abd9622d22f215c2959a9e
michael <michael.iodice@smartersorting.com>
2024-10-07 16:19:57 -0600

    fix: invert, led on when pin low

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -4,17 +4,17 @@
 void setup() {
   // Set the LED pin as an output
   pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH); // Turn off the LED
+  digitalWrite(LED_PIN, LOW); // Turn on the LED (pin low)
   delay(5000);                 // short button press
 }
 
 void loop() {
   // Turn the LED on
-  digitalWrite(LED_PIN, LOW);  // ESP-01 LED is active LOW
+  digitalWrite(LED_PIN, HIGH);  // Turn off LED, (pin high)
   delay(100);                 // short button press
 
   // Turn the LED off
-  digitalWrite(LED_PIN, HIGH); // Turn off the LED
+  digitalWrite(LED_PIN, LOW); // Turn on the LED (pin low)
 
   // sleep from 2 - 13 seconds
   delay(random(2000, 13001));
